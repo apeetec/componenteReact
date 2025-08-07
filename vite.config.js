ccee012ff9e951655211c3105a967fe25e9d5cd0
Gabriel <gabriel938@gmail.com>
2025-08-07 14:26:07 -0300

    Projeto de avaliação

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/projeto/', // <-- adicione esta linha com o nome do seu repositório
   plugins: [
     react(),
-  tailwindcss(),
+    tailwindcss(),
   ],
 })
