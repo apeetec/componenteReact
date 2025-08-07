75947ed1f68bf1682231b04edf8495f6051c526d
Gabriel <gabriel938@gmail.com>
2025-08-07 14:48:43 -0300

    Projeto de avaliação

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/projeto/', // <-- adicione esta linha com o nome do seu repositório
+  base: '/avalicaoGRI/', // <-- adicione esta linha com o nome do seu repositório
   plugins: [
     react(),
     tailwindcss(),
