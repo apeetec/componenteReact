5330222ec6531b5f6502ab64aa0aefc7057c865a
Gabriel <gabriel938@gmail.com>
2025-08-19 15:29:56 -0300

    Commit inicial

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/projeto/', // <- nome do repositório aqui, com barra no início e no fim
+  base: '/componenteReact/', // <- nome do repositório aqui, com barra no início e no fim
   plugins: [react(), tailwindcss()],
 })
